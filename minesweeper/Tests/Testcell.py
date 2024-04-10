b2bbfe9dc6a34ee49fe3a34303ded9463b8034e2
Alejandro montero <alejandro.montero@ucb.edu.bo>
2024-04-10 18:30:26 -0400

    tests creados

---
+++ minesweeper/Tests/Testcell.py
@@ -0,0 +1,18 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from cell import Cell
+
+class TestCell(unittest.TestCase):
+    def setUp(self):
+        self.cell = Cell(0,0)
+    
+    def test_next_mark(self):
+        self.cell.state = "flagged"
+        self.cell.next_mark()
+        self.assertEqual(self.cell.state, "questioned")
+        self.assertEqual(self.cell.int_state, 11)
+
+if __name__ == '__main__':
+    unittest.main()
