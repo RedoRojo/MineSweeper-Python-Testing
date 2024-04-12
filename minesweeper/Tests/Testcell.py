89311a5c18a77538160a58774193cd91b93affa5
RedoRojo <cristhiabnvictorrm@gmail.com>
2024-04-11 20:44:11 -0400

    open_cell test implemented

--- minesweeper/Tests/Testcell.py
+++ minesweeper/Tests/Testcell.py
@@ -1,12 +1,17 @@
 import unittest
 import sys
-sys.path.append('..')
+sys.path.append('../')
 
 from cell import Cell
 
 class TestCell(unittest.TestCase):
     def setUp(self):
         self.cell = Cell(0,0)
+
+    def test_open_cell_flagged(self): 
+        self.cell.state = "flagged"
+        self.cell.open()
+        self.assertEqual(self.cell.state, "flagged"); 
     
     def test_next_mark(self):
         self.cell.state = "flagged"
