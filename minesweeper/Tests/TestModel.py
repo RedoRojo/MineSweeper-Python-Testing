32e061982aebe8bdf8a18a4abf509ace98439c18
RedoRojo <cristhiabnvictorrm@gmail.com>
2024-04-13 19:53:25 -0400

    Get the field width of the model is tested

--- minesweeper/Tests/TestModel.py
+++ minesweeper/Tests/TestModel.py
@@ -8,11 +8,15 @@ from controller import Controller
 class TestModel(unittest.TestCase): 
     def setUp(self) -> None:
         self.model = Model()
+        self.model.FIELD_WIDTH = 100
 
     def test_set_controller(self): 
         controller = Controller(self.model)
         self.model.set_controller(controller)
         self.assertIsInstance(self.model.controller, Controller)
+
+    def test_get_field_width(self): 
+        self.assertEqual(self.model.get_FIELD_WIDTH(), 100)
 
     
 if __name__ == '__main__': 
