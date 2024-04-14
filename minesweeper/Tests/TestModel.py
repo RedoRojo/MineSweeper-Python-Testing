33d5c743d2f41145fd6901b5244ae7026464a8c1
RedoRojo <cristhiabnvictorrm@gmail.com>
2024-04-13 20:50:04 -0400

    Create a field of a model tested

--- minesweeper/Tests/TestModel.py
+++ minesweeper/Tests/TestModel.py
@@ -4,11 +4,11 @@ sys.path.append('../')
 
 from model import Model
 from controller import Controller
+from cell import Cell
 
 class TestModel(unittest.TestCase): 
     def setUp(self) -> None:
         self.model = Model()
-        self.model.FIELD_WIDTH = 100
 
     def test_set_controller(self): 
         controller = Controller(self.model)
@@ -16,6 +16,7 @@ class TestModel(unittest.TestCase):
         self.assertIsInstance(self.model.controller, Controller)
 
     def test_get_field_width(self): 
+        self.model.FIELD_WIDTH = 100
         self.assertEqual(self.model.get_FIELD_WIDTH(), 100)
 
     def test_get_field_height(self): 
@@ -26,6 +27,25 @@ class TestModel(unittest.TestCase):
         self.model.MINES_MAX = 60
         self.assertEqual(self.model.get_MINES_MAX(), 60)
 
+    def test_create_field_full_of_mines(self):
+        self.model.FIELD_HEIGHT = 10
+        self.model.FIELD_WIDTH = 10
+        self.model.MINES_MAX = 100
+        
+        self.model.create_field()
+        self.assertIsInstance(self.model.field, list)
+        self.assertIsInstance(self.model.field[0], list)
+        self.assertIsInstance(self.model.field[0][0], Cell)
+
+    def test_create_normal_field(self): 
+        self.model.FIELD_HEIGHT = 20
+        self.model.FIELD_WIDTH = 10
+        self.model.MINES_MAX = 100
+        
+        self.model.create_field()
+        self.assertIsInstance(self.model.field, list)
+        self.assertIsInstance(self.model.field[0], list)
+        self.assertIsInstance(self.model.field[0][0], Cell)
     
 if __name__ == '__main__': 
     unittest.main()
