7340b13deb319db137b4a12f51690ceb1d65734b
Alejandro montero <alejandro.montero@ucb.edu.bo>
2024-04-17 15:20:00 -0400

    many controller tests were added

--- minesweeper/Tests/Testcontroller.py
+++ minesweeper/Tests/Testcontroller.py
@@ -30,7 +30,7 @@ class TestController(unittest.TestCase):
         self.controller.left_click(0,0)
         self.view_mock.top_box.top_panel.start_btn.set_won.assert_called_once()
     
-    def test_left_click_win(self):
+    def test_left_click_lose(self):
         self.model_mock = MagicMock()
         self.model_mock.game_status.return_value = "Lose"
         self.view_mock = MagicMock()
@@ -52,6 +52,74 @@ class TestController(unittest.TestCase):
         self.controller.set_view(self.view_mock)
         self.controller.right_click(1,1)
         self.view_mock.top_box.top_panel.board.set.assert_called_once()
+    
+    def test_set_start_button(self):
+        self.model_mock = MagicMock()
+        self.view_mock = MagicMock()
+        self.controller = Controller(self.model_mock)
+        self.controller.view = self.view_mock
+        self.controller.set_start_button()
+        self.view_mock.top_box.top_panel.start_btn.set_start.assert_called_once()
+    
+    def test_set_win_button(self):
+        self.model_mock = MagicMock()
+        self.view_mock = MagicMock()
+        self.controller = Controller(self.model_mock)
+        self.controller.view = self.view_mock
+        self.controller.set_win_button()
+        self.view_mock.top_box.top_panel.start_btn.set_won.assert_called_once()
+    
+    def test_set_mines_board(self):
+        self.model_mock = MagicMock()
+        self.view_mock = MagicMock()
+        self.controller = Controller(self.model_mock)
+        self.controller.view = self.view_mock
+        mines = 10
+        self.controller.set_mines_board(mines)
+        self.view_mock.top_box.top_panel.board.set.assert_called_with(mines)
+        self.view_mock.update.assert_called_once()
+    
+    def test_get_status(self):
+        self.model_mock = MagicMock()
+        self.model_mock.game_status.return_value = "Win"
+        self.controller = Controller(self.model_mock)
+        status = self.controller.get_status()
+        self.assertEqual(status, "Win")
+    
+    def test_get_field_width(self):
+        self.model_mock = MagicMock()
+        self.model_mock.get_FIELD_WIDTH.return_value = 10
+        self.controller = Controller(self.model_mock)
+        width = self.controller.get_field_width()
+        self.assertEqual(width, 10)
+    
+    def test_get_field_height(self):
+        self.model_mock =  MagicMock()
+        self.model_mock.get_FIELD_HEIGHT.return_value = 10
+        self.controller = Controller(self.model_mock)
+        height = self.controller.get_field_height()
+        self.assertEqual(height, 10)
+    
+    def test_get_mines_max(self):
+        self.model_mock = MagicMock()
+        self.model_mock.get_MINES_MAX.return_value = 10
+        self.controller = Controller(self.model_mock)
+        mines_max = self.controller.get_mines_max()
+        self.assertEqual(mines_max, 10)
+    
+    def test_get_field(self):
+        self.model_mock = MagicMock()
+        self.controller = Controller(self.model_mock)
+        field = self.controller.get_field()
+        self.model_mock.get_field.assert_called_once()
+        self.assertEqual(field, self.model_mock.get_field())
+    
+    def test_get_seconds(self):
+        self.model_mock = MagicMock()
+        self.model_mock.get_seconds_from_start.return_value = 10
+        self.controller = Controller(self.model_mock)
+        seconds = self.controller.get_seconds()
+        self.assertEqual(seconds, 10)
 
     def test_start_new_game(self): 
         self.model_mock = MagicMock()
@@ -70,6 +138,7 @@ class TestController(unittest.TestCase):
         self.controller.start_new_game_smile(); 
         self.model_mock.new_game.assert_called_once()
         self.view_mock.update.assert_called_once()
+    
 
     def test_start_new_game_easy(self): 
         self.model_mock = MagicMock()
